4b9108c5c854be00b44d30120efb3e7e7c86c31f
ShanYu-bioinfo <yushan2519@outlook.com>
2025-05-05 13:06:39 -0400

    Add files via upload

---
+++ modules/combine_all_sample_PAS_count_tables.R
@@ -0,0 +1,27 @@
+
+if (!require('tidyverse')) install.packages('tidyverse'); library('tidyverse')
+
+
+args<- commandArgs(TRUE)
+args_v<-NA
+if(length(args)>1){
+   args_v <- args[c(FALSE, TRUE)]
+   names(args_v) <- sub("\\-+","", args[c(TRUE, FALSE)])
+   print(args_v)
+}
+args<-NULL
+
+csv_dir <- ifelse(is.na(args_v["csv"]), "_csv_", args_v["csv"])
+out_dir <- ifelse(is.na(args_v["out"]), "_out_", args_v["out"])
+
+
+filenames <- list.files(csv_dir, pattern="*.csv", full.names=TRUE)
+ldf <- lapply(filenames, read.csv)
+
+cname=colnames(ldf[[1]])[1] # sy
+cluster.all.reads <- Reduce(function(x, y) merge(x, y, by=cname, all=T,sort=F), ldf) # sy
+cluster.all.reads[is.na(cluster.all.reads)] <- 0
+
+#cluster.all.reads <- cluster.all.reads %>% separate(PasID, sep = ":", into = c("chromosome","strand","position"), remove = T) #sy
+
+write.csv(cluster.all.reads, out_dir, row.names = F, quote = F)
